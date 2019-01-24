8ddcc42756f7674db2bed68a5120279bbb25c495
Jack Pines <jackpi@microsoft.com>
2019-01-24 14:05:49 -0800

    Completes Heap implementations for both max and min.

--- CollectionsFromScratch/MaxHeap.cs
+++ CollectionsFromScratch/MaxHeap.cs
@@ -1,84 +1,83 @@
-using System;
 using System.Collections.Generic;
-
+using System;
 namespace Study.CollectionsFromScratch
 {
-    public class MinHeap<TValue> : IHeap<TValue> where TValue : IComparable<TValue>
+    public class MaxHeap<TValue> : IHeap<TValue> where TValue : IComparable<TValue>
     {
-        MinHeap<TValue, TValue> minHeap;
+        MaxHeap<TValue, TValue> maxHeap;
 
-        public MinHeap()
+        public MaxHeap()
         {
-            this.minHeap = new MinHeap<TValue, TValue>();
+            this.maxHeap = new MaxHeap<TValue, TValue>();
         }
 
-        public int Size { get { return this.minHeap.Size; } }
-
-        public bool IsEmpty()
-        {
-            return this.minHeap.IsEmpty();
-        }
+        public int Size { get { return this.maxHeap.Size; } }
 
         public void Insert(TValue value)
         {
-            this.minHeap.Insert(value, value);
+            this.maxHeap.Insert(value, value);
+        }
+
+        public bool IsEmpty()
+        {
+            return this.maxHeap.IsEmpty();
         }
 
         public TValue Peek()
         {
-            return this.minHeap.Peek();
+            return this.maxHeap.Peek();
         }
 
         public TValue Pop()
         {
-            return this.minHeap.Pop();
+            return this.maxHeap.Pop();
         }
     }
 
-    public class MinHeap<TPriority, TValue> : IHeap<TPriority, TValue> where TPriority : IComparable<TPriority>
+    public class MaxHeap<TPriority, TValue> : IHeap<TPriority, TValue> where TPriority : IComparable<TPriority>
     {
-        List<KeyValuePair<TPriority, TValue>> minHeap;
+        List<KeyValuePair<TPriority, TValue>> maxHeap;
 
-        public MinHeap()
+        public MaxHeap()
         {
-            this.minHeap = new List<KeyValuePair<TPriority, TValue>>();
+            this.maxHeap = new List<KeyValuePair<TPriority, TValue>>();
         }
 
         public int Size { get; private set; }
 
-        public bool IsEmpty()
-        {
-            return this.Size == 0;
-        }
-
         public void Insert(TPriority priority, TValue value)
         {
             // Heap inserts are always to the end of the list
-            this.minHeap.Add(new KeyValuePair<TPriority, TValue>(priority, value));
+            this.maxHeap.Add(new KeyValuePair<TPriority, TValue>(priority, value));
             this.Size++;
 
             // Then we swim that last item up to its appropriate location in the heap
             this.Swim();
         }
 
+        public bool IsEmpty()
+        {
+            return this.Size == 0;
+        }
+
         public TValue Peek()
         {
-            // In min-heap, first item is always smallest
-            return this.minHeap[0].Value;
+            // In max-heap, first item is always largest
+            return this.maxHeap[0].Value;
         }
 
         public TValue Pop()
         {
-            var currentMin = Peek();
+            TValue maxValue = Peek();
 
             // Move last item into first position and delete the last item from list
-            this.minHeap[0] = this.minHeap[--this.Size];
-            this.minHeap.RemoveAt(this.Size);
-            
+            this.maxHeap[0] = this.maxHeap[--this.Size];
+            this.maxHeap.RemoveAt(this.Size);
+
             // Sink the new first item to its appropriate location in heap
             this.Sink();
 
-            return currentMin;
+            return maxValue;
         }
 
         void Sink()
@@ -88,27 +87,27 @@ namespace Study.CollectionsFromScratch
             while (this.HasLeftChild(currentIndex))
             {
                 int indexOfChildToCompare = this.GetLeftChildIndex(currentIndex);
-                KeyValuePair<TPriority, TValue> childToCompare = this.minHeap[indexOfChildToCompare];
+                KeyValuePair<TPriority, TValue> childToCompare = this.maxHeap[indexOfChildToCompare];
 
                 if (this.HasRightChild(currentIndex))
                 {
                     int rightChildIndex = this.GetRightChildIndex(currentIndex);
-                    KeyValuePair<TPriority, TValue> rightChild = this.minHeap[rightChildIndex];
+                    KeyValuePair<TPriority, TValue> rightChild = this.maxHeap[rightChildIndex];
 
-                    if (rightChild.Key.CompareTo(childToCompare.Key) < 0)
+                    if (rightChild.Key.CompareTo(childToCompare.Key) > 0)
                     {
                         indexOfChildToCompare = rightChildIndex;
                         childToCompare = rightChild;
                     }
                 }
 
-                if (this.minHeap[currentIndex].Key.CompareTo(childToCompare.Key) < 0)
+                if (this.maxHeap[currentIndex].Key.CompareTo(childToCompare.Key) > 0)
                 {
                     return;
                 }
 
-                this.minHeap[indexOfChildToCompare] = this.minHeap[currentIndex];
-                this.minHeap[currentIndex] = childToCompare;
+                this.maxHeap[indexOfChildToCompare] = this.maxHeap[currentIndex];
+                this.maxHeap[currentIndex] = childToCompare;
                 currentIndex = indexOfChildToCompare;
             }
         }
@@ -121,13 +120,13 @@ namespace Study.CollectionsFromScratch
             {
                 int parentIndex = this.GetParentIndex(currentIndex);
 
-                KeyValuePair<TPriority, TValue> current = this.minHeap[currentIndex];
-                KeyValuePair<TPriority, TValue> parent = this.minHeap[parentIndex];
+                KeyValuePair<TPriority, TValue> current = this.maxHeap[currentIndex];
+                KeyValuePair<TPriority, TValue> parent = this.maxHeap[parentIndex];
                 
-                if (parent.Key.CompareTo(current.Key) <= 0) { return; }
+                if (parent.Key.CompareTo(current.Key) >= 0) { return; }
 
-                this.minHeap[currentIndex] = parent;
-                this.minHeap[parentIndex] = current;
+                this.maxHeap[currentIndex] = parent;
+                this.maxHeap[parentIndex] = current;
                 currentIndex = parentIndex;
             }
         }
@@ -139,7 +138,7 @@ namespace Study.CollectionsFromScratch
 
         int GetLeftChildIndex(int currentIndex)
         {
-            return 2 * currentIndex + 1;
+            return currentIndex * 2 + 1;
         }
 
         bool HasRightChild(int currentIndex)
@@ -149,7 +148,7 @@ namespace Study.CollectionsFromScratch
 
         int GetRightChildIndex(int currentIndex)
         {
-            return 2 * currentIndex + 2;
+            return currentIndex * 2 + 2;
         }
 
         bool HasParent(int currentIndex)
@@ -161,4 +160,5 @@ namespace Study.CollectionsFromScratch
         {
             return (currentIndex - 1) / 2;
         }
-    }}
+    }
+}
--- CollectionsFromScratchTests/HeapTests.cs
+++ CollectionsFromScratchTests/HeapTests.cs
@@ -13,9 +13,12 @@ namespace Study.CollectionsFromScratch.Tests
             var testData = new [] { 5, 4, 3, 2, 1 };
             
             IHeap<int> minHeap = new MinHeap<int>();
+            IHeap<int> maxHeap = new MaxHeap<int>();
+
             foreach (var item in testData)
             {
                 minHeap.Insert(item);
+                maxHeap.Insert(item);
             }
         }
 
@@ -25,14 +28,16 @@ namespace Study.CollectionsFromScratch.Tests
             var testData = new [] { 5, 4, 3, 2, 1 };
             
             IHeap<int> minHeap = new MinHeap<int>();
+            IHeap<int> maxHeap = new MaxHeap<int>();
+
             foreach (var item in testData)
             {
                 minHeap.Insert(item);
+                maxHeap.Insert(6 - item);
             }
 
-            var actual = minHeap.Peek();
-
             Assert.AreEqual(1, minHeap.Peek());
+            Assert.AreEqual(5, maxHeap.Peek());
         }
 
         [TestMethod]
@@ -41,14 +46,18 @@ namespace Study.CollectionsFromScratch.Tests
             var testData = new [] { 5, 4, 3, 2, 1 };
 
             IHeap<int> minHeap = new MinHeap<int>();
-
             Assert.IsTrue(minHeap.IsEmpty());
+
+            IHeap<int> maxHeap = new MaxHeap<int>();
+            Assert.IsTrue(maxHeap.IsEmpty());
 
             foreach (var item in testData)
             {
                 minHeap.Insert(item);
-
                 Assert.IsFalse(minHeap.IsEmpty());
+
+                maxHeap.Insert(item);
+                Assert.IsFalse(maxHeap.IsEmpty());
             }
         }
 
@@ -58,15 +67,19 @@ namespace Study.CollectionsFromScratch.Tests
             var testData = new [] { 5, 4, 3, 2, 1 };
 
             IHeap<int> minHeap = new MinHeap<int>();
+            IHeap<int> maxHeap = new MaxHeap<int>();
 
             for (int i = 0; i < testData.Length; i++)
             {
                 Assert.AreEqual(i, minHeap.Size);
-
                 minHeap.Insert(testData[i]);
+
+                Assert.AreEqual(i, maxHeap.Size);
+                maxHeap.Insert(testData[i]);
             }
 
             Assert.AreEqual(testData.Length, minHeap.Size);
+            Assert.AreEqual(testData.Length, maxHeap.Size);
         }
 
         [TestMethod]
@@ -75,14 +88,18 @@ namespace Study.CollectionsFromScratch.Tests
             var testData = new [] { 5, 4, 3, 2, 1 };
             
             IHeap<int> minHeap = new MinHeap<int>();
+            IHeap<int> maxHeap = new MaxHeap<int>();
+
             foreach (var item in testData)
             {
                 minHeap.Insert(item);
+                maxHeap.Insert(item);
             }
 
             for (int i = 1; i <= testData.Length; i++)
             {
                 Assert.AreEqual(i, minHeap.Pop());
+                Assert.AreEqual(6 - i, maxHeap.Pop());
             }
         }
     }
