287b0174460be3e719cbaf8a6a6741c13cdb5049
Jack Pines <jackpi@microsoft.com>
2019-01-25 16:09:49 -0800

    Clean up & adds AlgorithmsFromScratch + Tests

---
+++ AlgorithmsFromScratch/Sorting/HeapSort.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+using CollectionsFromScratch;
+
+namespace AlgorithmsFromScratch.Sorting
+{
+    public static class HeapSort<TValue> where TValue : IComparable<TValue>
+    {
+        public static void Sort(TValue[] values)
+        {
+            values = SortInternal(values).ToArray();
+        }
+
+        public static void Sort(List<TValue> values)
+        {
+            values = SortInternal(values).ToList();
+        }
+
+        static IList<TValue> SortInternal(IList<TValue> values)
+        {
+            IHeap<TValue> heap = new Heap<TValue>();
+            
+            foreach (TValue value in values)
+            {
+                heap.Insert(value);
+            }
+
+            TValue[] result = new TValue[heap.Size];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = heap.Pop();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AlgorithmsFromScratchTests/HeapSortTests.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using AlgorithmsFromScratch.Sorting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsFromScratchTests
+{
+    [TestClass]
+    public class HeapSortTests
+    {
+        [TestMethod]
+        public void HeapSortListSortsMinToMax()
+        {
+            List<int> values = this.CreateRandomizedListOfValues();
+
+            HeapSort<int>.Sort(values);
+
+            AssertMinToMax(values);
+        }
+
+        [TestMethod]
+        public void HeapSortArraySortsMinToMax()
+        {
+            int[] values = this.CreateRandomizedListOfValues().ToArray();
+
+            HeapSort<int>.Sort(values);
+
+            AssertMinToMax(values);
+        }
+
+        void AssertMinToMax(IList<int> values)
+        {
+            int currentValue = values[0];
+            for (int i = 1; i < values.Count; i++)
+            {
+                int nextValue = values[i];
+                Assert.IsTrue(currentValue <= nextValue);
+                currentValue = nextValue;
+            }
+        }
+
+        List<int> CreateRandomizedListOfValues(int maxSize = 10)
+        {
+            var values = new List<int>();
+
+            var rnd = new Random();
+
+            for (int i = 0; i < maxSize; i++)
+            {
+                values.Add(rnd.Next());
+            }
+
+            return values;
+        }
+    }
+}
